fd71932bafe752dbf11527a9f5575550f0b4610b
MonoToast <monotoastjlr@gmail.com>
2025-10-06 03:36:34 -0500

    Add project files.

---
+++ Space Duct Tape/Main.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Space_Duct_Tape
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string GUID = "com.monotoast.Space_Duct_Tape";
+        public const string NAME = "Space_Duct_Tape";
+        public const string VERSION = "1.0.0";
+
+        private Harmony _harmony;
+
+        private void Awake()
+        {
+            Logger.LogInfo($"{NAME} v{VERSION} is loading...");
+
+            _harmony = new Harmony(GUID);
+
+            try
+            {
+                _harmony.PatchAll();
+                Logger.LogInfo($"{NAME} patches applied successfully.");
+            }
+            catch (System.Exception ex)
+            {
+                Logger.LogError($"Error patching: {ex}");
+            }
+
+            Logger.LogInfo($"{NAME} v{VERSION} has loaded.");
+        }
+
+        private void OnDestroy()
+        {
+            try
+            {
+                _harmony?.UnpatchSelf();
+                Logger.LogInfo($"{NAME} patches have been removed.");
+            }
+            catch (System.Exception ex)
+            {
+                Logger.LogError($"Error unpatching: {ex}");
+            }
+        }
+    }
+}
